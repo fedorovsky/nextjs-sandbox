494445cb904a2c21cd2bdfd42735245a9622b627
a.fedorovskyi <anton.fedorovsky@gmail.com>
2025-08-17 19:43:45 +0200

    added isr

---
+++ app/isr/page.tsx
@@ -0,0 +1,37 @@
+/**
+ * This is an example of a page that uses Incremental Static Regeneration (ISR).
+ * It will be regenerated every 10 seconds.
+ * The page will display the last generated time.
+ */
+export const revalidate = 10;
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default async function ISR() {
+  const now = new Date().toISOString();
+
+  /**
+   * The `next: { revalidate: 10 }` option tells Next.js to
+   * revalidate this page every 10 seconds.
+   */
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5", {
+    next: { revalidate: 10 },
+  });
+
+  const posts: Post[] = await res.json();
+
+  return (
+    <div>
+      <h2>ISR Page</h2>
+      <p>Last generated at: {now}</p>
+      <ul>
+        {posts.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h2>Home Page</h2>
+      <ul>
+        <li>
+          <Link href="/isr">Go to ISR page</Link>
+        </li>
+      </ul>
     </div>
   );
 }
