8d234e5d29ff6020c610e995a44db2e6f76f8c7e
a.fedorovskyi <anton.fedorovsky@gmail.com>
2025-08-17 18:47:43 +0200

    cleared the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +12,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <h1>Jenkins - 4</h1>
-        <h2>Build: {process.env.NEXT_PUBLIC_COMMIT_SHA}</h2>
+      <body>
+        <h2>Build: {process.env.NEXT_PUBLIC_COMMIT_SHA || 'dev'}</h2>
         {children}
       </body>
     </html>
