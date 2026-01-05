0af4d655a82e7102ab11b05e04d6353044b1f352
a.fedorovskyi <anton.fedorovsky@gmail.com>
2026-01-05 20:12:58 +0100

    comments

--- app/isr/page.tsx
+++ app/isr/page.tsx
@@ -1,18 +1,24 @@
 import type { Post } from '@/app/isr/types';
 
 /**
- * This is an example of a page that uses Incremental Static Regeneration (ISR).
- * It will be regenerated every 10 seconds.
- * The page will display the last generated time.
+ * Regenerate the page HTML after 10 seconds
+ * when the next HTTP request to this page arrives.
+ * (Incremental Static Regeneration) ISR
  */
 export const revalidate = 10;
 
 export default async function ISR() {
-  const now = new Date().toISOString();
+  /**
+   * Time when this HTML was generated.
+   * Changes ONLY when the page is regenerated,
+   * not on every request.
+   */
+  const now = new Date().toString();
 
   /**
-   * The `next: { revalidate: 10 }` option tells Next.js to
-   * revalidate this page every 10 seconds.
+   * Fetch posts and cache the response.
+   * The cached data is refreshed every 10 seconds.
+   * Keeps data in sync with page regeneration.
    */
   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5', {
     next: { revalidate: 10 },
